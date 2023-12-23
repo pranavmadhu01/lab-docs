aee6fcd11a3953089c9c5b21f7666f01e067b3cf
Pranav Madhu <pranavmadhu73@gmail.com>
2023-12-23 21:25:50 +0530

    changes in configuration in theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,46 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>Lab Docs</span>,
+  useNextSeoProps() {
+    return{
+      titleTemplate: '%s – Lab Docs'
+    }
+  },
+  banner: {
+    key: 'release',
+    text: (
+      <a href="https://github.com/pranavmadhu01/lab-docs" target="_blank">
+        🔴 Lab docs is looking for contributers from different universities. 🔴
+      </a>
+    )
+  },
+  logo: (
+    <>
+      <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" id="code">
+        <path
+          fill="#444"
+          d="M11.067 10.423 6.25 16.286l4.817 5.862 1.139-1.874-3.246-3.989 3.246-3.989zm2.108 11.585h2.114l3.361-11.477h-2.115zm7.758-11.585-1.139 1.874 3.246 3.989-3.246 3.989 1.139 1.874 4.817-5.862z"
+        ></path>
+      </svg>
+      <span style={{ marginLeft: ".4em", fontWeight: 500 }}>Lab Docs</span>
+    </>
+  ),
   project: {
-    link: 'https://github.com/pranavmadhu01/lab-docs',
+    link: "https://github.com/pranavmadhu01/lab-docs",
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/pranavmadhu01/lab-docs',
+  docsRepositoryBase: "https://github.com/pranavmadhu01/lab-docs",
   footer: {
-    text: 'Lab Docs',
+    text: (
+      <span>
+        Lab Docs {new Date().getFullYear()} ©{' '}
+        <a href="https://pranavmadhu.in" target="_blank">
+          pranavmadhu
+        </a>
+        .
+      </span>
+    ),
   },
-}
+};
 
-export default config
+export default config;
